8b85bb3afe49d02c9d78457fcc9a9e9a0aabae3c
정소정 <thwjd0205@naver.com>
2023-07-24 15:45:38 +0900

    rename: Calendar -> Schedule

--- frontend/blooming/src/App.jsx
+++ frontend/blooming/src/App.jsx
@@ -3,7 +3,7 @@ import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from './Pages/Home'
 import Login from './Pages/Login'
-import Calendar from './Pages/Calendar';
+import Schedule from './Pages/Schedule';
 import { NavLink } from 'react-router-dom';
 
 function App() {
@@ -16,12 +16,12 @@ function App() {
         <ul>
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/Login">Login</NavLink></li>
-          <li><NavLink to="/Calendar">Calendar</NavLink></li>
+          <li><NavLink to="/Schedule">Schedule</NavLink></li>
         </ul>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Login" element={<Login />} />
-          <Route path="/Calendar" element={<Calendar />} />
+          <Route path="/Schedule" element={<Schedule />} />
         </Routes>
       </RecoilRoot>
     </Router>
