8eaa06f78752c7c1a7b96a35228f5d2df50c7c21
정소정 <thwjd0205@naver.com>
2023-07-24 15:47:31 +0900

    rename: Calendar.jsx -> Schedule.jsx

---
+++ frontend/blooming/src/Pages/Schedule.jsx
@@ -0,0 +1,12 @@
+// import Calendar from './components/Schedule/calendar';
+
+function Schedule() {
+  return (
+    <div>
+      {/* <Calendar /> */}
+      <h1>일단 캘린더다.</h1>
+    </div>
+  )
+}
+
+export default Schedule
